42be797f1d5e64f3bd988f9967b5cbe571764846
kvernert <kverner47@gmail.com>
2022-09-15 18:43:28 -0300

    update

--- jogo da velha/index.js
+++ jogo da velha/index.js
@@ -1,6 +1,9 @@
 
 let jogador = "X";
 
+let vitoriaX = 0;
+let vitoriaO = 0;
+
 let casa01 = document.getElementById("casa01");
 let casa02 = document.getElementById("casa02");
 let casa03 = document.getElementById("casa03");
@@ -33,37 +36,54 @@ function alterna_jogador() {
     }
 }
 function verifica_ganhador() {
-    if (casa01.innerHTML!="" && casa01.innerHTML===casa02.innerHTML && casa01.innerHTML=== casa03.innerHTML){
-        alert(`Temos uma ganhador: ${casa01.innerHTML}`);
+     if(casa01.innerHTML != "" && casa01.innerHTML === casa02.innerHTML && casa01.innerHTML  === casa03.innerHTML){
+        alert(`Vencedor: ${casa01.innerHTML}`);
+        placar(casa01.innerHTML);
+    } else if (casa04.innerHTML != "" && casa04.innerHTML === casa05.innerHTML && casa04.innerHTML === casa06.innerHTML) {
+        alert (`Vencedor: ${casa04.innerHTML}`);
+        placar(casa04.innerHTML);
+    } else if (casa07.innerHTML != "" && casa07.innerHTML === casa08.innerHTML && casa07.innerHTML === casa09.innerHTML) {
+        alert (`Vencedor: ${casa07.innerHTML}`);
+        placar(casa07.innerHTML);
+    } else if (casa01.innerHTML != "" && casa01.innerHTML === casa04.innerHTML && casa01.innerHTML === casa07.innerHTML) {
+        alert (`Vencedor: ${casa01.innerHTML}`);
+        placar(casa01.innerHTML);
+    } else if (casa02.innerHTML != "" && casa02.innerHTML === casa05.innerHTML && casa02.innerHTML === casa08.innerHTML) {
+        alert (`Vencedor: ${casa02.innerHTML}`);
+        placar(casa02.innerHTML);
+    } else if (casa03.innerHTML != "" && casa03.innerHTML === casa06.innerHTML && casa03.innerHTML === casa09.innerHTML) {
+        alert (`Vencedor: ${casa03.innerHTML}`);
+        placar(casa03.innerHTML);
+    } else if (casa01.innerHTML != "" && casa01.innerHTML === casa05.innerHTML && casa01.innerHTML === casa09.innerHTML) {
+        alert (`Vencedor: ${casa01.innerHTML}`);
+        placar(casa01.innerHTML);
+    } else if (casa03.innerHTML != "" && casa03.innerHTML === casa05.innerHTML && casa03.innerHTML === casa07.innerHTML) {
+        alert (`Vencedor: ${casa03.innerHTML}`);
+        placar(casa03.innerHTML);
+    } else if (casa01.innerHTML != "" && casa02.innerHTML != "" && casa03.innerHTML != "" && casa04.innerHTML != "" && casa05.innerHTML != "" && casa06.innerHTML != "" && casa07.innerHTML != "" && casa08.innerHTML != "" && casa09.innerHTML != "")
+        alert (`Deu velha!`);
     }
-    else if(casa04.innerHTML != "" && casa04.innerHTML === casa05.innerHTML && casa04.innerHTML === casa06.innerHTML){
-        alert(`Temos um vencedor: ${casa04.innerHTML}`);
-     }    
-     else if (casa07.innerHTML != "" && casa07.innerHTML === casa08.innerHTML && casa07.innerHTML === casa09.innerHTML){
-        alert(`Temos um vencedor: ${casa07.innerHTML}`);
-     }
-     else if (casa01.innerHTML != "" && casa01.innerHTML == casa04.innerHTML && casa01.innerHTML === casa07.innerHTML){
-        alert(`Temos um vencedor: ${casa01.innerHTML}`);
-     }
-     else if (casa02.innerHTML != "" && casa02.innerHTML == casa05.innerHTML && casa02.innerHTML === casa08.innerHTML){
-        alert(`Temos um vencedor: ${casa02.innerHTML}`);
-     }
-     else if (casa03.innerHTML != "" && casa03.innerHTML == casa06.innerHTML && casa03.innerHTML === casa09.innerHTML){
-        alert(`Temos um vencedor: ${casa03.innerHTML}`);
-     }
-     else if (casa01.innerHTML != "" && casa01.innerHTML == casa05.innerHTML && casa01.innerHTML === casa09.innerHTML){
-        alert(`Temos um vencedor: ${casa01.innerHTML}`);
-     }
-     else if (casa03.innerHTML != "" && casa03.innerHTML == casa05.innerHTML && casa03.innerHTML === casa07.innerHTML){
-        alert(`Temos um vencedor: ${casa03.innerHTML}`);
-     }
+function placar(vencedor) {
+    casa01.innerHTML = "";
+    casa02.innerHTML = "";
+    casa03.innerHTML = "";
+    casa04.innerHTML = "";
+    casa05.innerHTML = "";
+    casa06.innerHTML = "";
+    casa07.innerHTML = "";
+    casa08.innerHTML = "";
+    casa09.innerHTML = "";
+
+
+    vez = "X";
+
+
+    if (vencedor === "X"){
+        vitoriaX = vitoriaX + 1;
+
+    }else{
+        vitoriaO = vitoriaO + 1;
+    }
+    document.getElementById("resultado").innerHTML = `<h1>Vitórias</h1> <p>X: ${vit_X}</p> <br> 
+    <p>O: ${vit_O}</p>`
 }
-function venceu(){
-    
-}
-
-
-
-
-
-
