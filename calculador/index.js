a85b90c486e95a7168d07c7f0f21707466e13886
kvernert <kverner47@gamil.com>
2022-10-07 14:20:05 -0300

    update

--- calculador/index.js
+++ calculador/index.js
@@ -4,60 +4,86 @@ let btnNumeros = document.getElementsByClassName("botao-numero");
 let btnOperador = document.getElementsByClassName("botao-operador");
 let btnApagar = document.getElementById("botaoApagaUm");
 
-let valor1 = null;
-let operador = null;
+let valorSalvo = null;
+let operadorSalvo = null;
 
 for (let botao of btnNumeros) {
-  botao.addEventListener("click", cliqueNumero);
+    botao.addEventListener("click", cliqueNumero);
 }
 
 function cliqueNumero(event) {
-    if (visor.innerHTML === "+")
-        visor.innerHTML = event.target.innerHTML;
-    else
+    if (isNaN(visor.innerHTML) === false)
         visor.innerHTML = visor.innerHTML + event.target.innerHTML;
+    else visor.innerHTML = event.target.innerHTML;
 }
 for (let botao of btnOperador) {
-  botao.addEventListener("click", cliqueOperdaor);
+    botao.addEventListener("click", cliqueOperdaor);
 }
-
-function realiza_operacao(operador, valor1, valor2){
-    if (operador === "+"){
-        return valor1+valor2;
-    }
-    else if(operador === "-"){
-        return valor1 - valor2;
-    } 
-    else if(operador === "÷"){
-        return valor1 / valor2;   
-    }
-    else if (operador === "×"){
-        return valor1 * valor2
-    }
 function cliqueOperdaor(event) {
-
-    if (valor1 === null){
-        valor1 = Number(visor.innerHTML);
-        operador = event.target.innerHTML;
-        visor.innerHTML = event.target.innerHTML;
-    }else{
-        let valor2 = Number(visor.innerHTML);
-        visor.innerHTML = realiza_operacao(operador, valor1, valor2);
-        valor1 = Number(visor.innerHTML);
-
-
+    if (isNaN(visor.innerHTML) === false) {
+        if (valorSalvo === null) {
+            valorSalvo = Number(visor.innerHTML);
+        } else {
+            valorSalvo = executaOperacao(
+                valorSalvo,
+                operadorSalvo,
+                Number(visor.innerHTML)
+            );
+        }
     }
-  
+
+    operadorSalvo = event.target.innerHTML;
+    visor.innerHTML = operadorSalvo;
 }
 let btnAC = document.getElementById("botaoAC");
 btnAC.addEventListener("click", limpaVisor);
 
 function limpaVisor(event) {
-  visor.innerHTML = "";
+    visor.innerHTML = "";
+    valorSalvo = null;
+    operadorSalvo = null;
 }
 let btnApaga = document.getElementById("botaoApagaUm");
 btnApaga.addEventListener("click", limparUm);
 
 function limparUm(event) {
-  visor.innerHTML = visor.innerHTML.slice(0, visor.innerHTML.length - 1);
+    visor.innerHTML = visor.innerHTML.slice(0, visor.innerHTML.length - 1);
+}
+let botaoIgual = document.getElementById("botaoIgual");
+botaoIgual.addEventListener("click", calculaResultado);
+
+function calculaResultado(event) {
+    if (
+        valorSalvo != null &&
+        operadorSalvo != null &&
+        isNaN(visor.innerHTML) === false
+    ) {
+        visor.innerHTML = executaOperacao(valorSalvo, operadorSalvo, Number(visor.innerHTML));
+        valorSalvo = null;
+        operadorSalvo = null;
+    }
+}
+function executaOperacao(valor1, operador, valor2) {
+    if (operador === "×") {
+        return valor1 * valor2;
+    } else if (operador === "÷") {
+        return valor1 / valor2;
+    } else if (operador === "-") {
+        return valor1 - valor2;
+    } else if (operador === "+") {
+        return valor1 + valor2;
+    }
+}
+let body = document.getElementsByTagName("body")[0];
+body.addEventListener("keydown", pressionouTecla);
+function pressionouTecla(event) {
+    if (event.key === "×") {
+        document.getElementById("botaoMultiplicacao").click();
+    } else if (event.key === "÷") {
+        document.getElementById("botaoDivisao").click();
+    } else if (event.key === "-") {
+        document.getElementById("botaoMenos").click();
+    } else if (event.key === "+") {
+        document.getElementById("botaoMais").click();
+    }
 }
