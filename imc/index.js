c174e281300a03645051452339415e938dfa3b65
kvernert <kverner47@gmail.com>
2022-10-21 15:09:33 -0300

    por favorrrrrrr que css eu crio

---
+++ imc/index.js
@@ -0,0 +1,40 @@
+function calculeIMC(){
+    const nome = document.getElementById('peso').value;
+    const peso = Number(document.getElementById('peso').value);
+    const altura = Number(document.getElementById('altura').value);
+    const resultadoIMC = peso / (altura * altura);
+    document.getElementById("resultadoIMC").innerHTML = `${nome}: ${resultadoIMC.toFixed(2)}`;
+    let resposta;
+    let cor;
+
+    if(resultadoIMC <= 15.99){
+        resposta = `Magreza Grave <br/>`;
+        cor = 'perigo';
+    }
+    else if(resultadoIMC >= 16 && resultadoIMC <= 16.99){
+        resposta = `Magreza Moderada </br>`;
+        cor = 'alerta'
+    }
+    else if(resultadoIMC >= 17 && resultadoIMC <= 24.9){
+        resposta = `Saudável </br>`;
+        cor = 'deboa'
+    }
+    else if(resultadoIMC >= 25 && resultadoIMC <= 29.9){
+        resposta = `Sobrepeso </br>`;
+        cor = 'atencao'
+    }
+    else if(resultadoIMC >= 23 && resultadoIMC <= 34.9){
+        resposta = `Obesidade Grau 1 <br/>`;
+        cor = 'atencao'
+    }
+    else if(resultadoIMC >= 35 && resultadoIMC <= 39.9){
+        resposta = `Obesidade Grau 2`;
+        cor = 'alerta'
+    }
+    else if(resultadoIMC >= 40){
+        resposta = `Obesidade Grau 3`;
+        cor = 'perigo';
+    }
+    document.getElementById("resultadoCategoria").innerHTML = `Categoria = ${resposta}`
+    document.getElementById("resultadoCategoria").className = cor;
+}
